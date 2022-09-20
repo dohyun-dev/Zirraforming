e82bb150eb379e0d16f6d6323ce2c86d1eb9a880
박지현 <spy03128@naver.com>
2022-09-20 17:17:41 +0900

    S07P22D107-408 환경 상식 퀴즈 문제 화면

--- ZirraForming/client/src/Router.js
+++ ZirraForming/client/src/Router.js
@@ -3,18 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Mbti from "./pages/Mbti";
 import MbtiResult from "./pages/MbtiResult";
+import Quiz from "./pages/Quiz";
+import QuizResult from "./pages/QuizResult";
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/style" element={<Mbti />}></Route>
-        <Route path="/style/result" element={<MbtiResult />}></Route>
-        {/* //<Route path="/quiz" element={<Quiz />}></Route> */}
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />}></Route>
+				<Route path="/style" element={<Mbti />}></Route>
+				<Route path="/style/result" element={<MbtiResult />}></Route>
+				<Route path="/quiz" element={<Quiz />}></Route>
+				<Route path="/quiz/result" element={<QuizResult />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default Router;
