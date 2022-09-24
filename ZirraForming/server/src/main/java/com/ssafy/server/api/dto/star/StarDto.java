4d6e76c74cbaa641b35f08555f7e9648257dd50c
변수경 <workcom0@naver.com>
2022-09-24 23:55:46 +0900

    S07P22D107-447 별보러갈래 api 개발
    Former-commit-id: 46b35886e1b99d37fea792195e0536e71b4e12d9

---
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/dto/star/StarDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.server.api.dto.star;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StarDto {
+    private Long memberId;
+    private double co2;
+    private String imgUrl;
+}
