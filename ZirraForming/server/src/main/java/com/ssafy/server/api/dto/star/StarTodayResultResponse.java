e6f8a9512051dba9cbcb2383931936c5210c0032
권도현 <tobeykwon@gmail.com>
2022-10-03 16:19:09 +0900

    S07P22D107-616 별보러갈래 일별 결과 Response 수정
    Former-commit-id: e0baaabbed06fbbcc4b356ee18b5111316bd5db7

--- ZirraForming/server/src/main/java/com/ssafy/server/api/dto/star/StarTodayResultResponse.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/dto/star/StarTodayResultResponse.java
@@ -3,12 +3,14 @@ package com.ssafy.server.api.dto.star;
 import com.ssafy.server.domain.dto.PredictResultResponse;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class StarTodayResultResponse {
-    private Map<String, Integer> trashCount;
+    private List<String> trashType = new ArrayList<>();
+    private List<Integer> trashCount = new ArrayList<>();
     private String temperature_2030;
     private String ice_2030;
     private String co2_2030;
@@ -18,12 +20,20 @@ public class StarTodayResultResponse {
 
     public StarTodayResultResponse(Map<String, Object> todayResult) {
         PredictResultResponse predictResult = (PredictResultResponse) todayResult.get("predictResult");
-        this.trashCount = (Map<String, Integer>) todayResult.get("trashCount");
-        this.temperature_2030 = String.format("%.2f", predictResult.getIce_2030());
+        makeTrashResult(todayResult);
+        this.temperature_2030 = String.format("%.2f", predictResult.getTemperature_2030());
         this.ice_2030 = String.format("%.2f", predictResult.getIce_2030());
         this.co2_2030 = String.format("%.2f", predictResult.getCo2_2030());
         this.year = predictResult.getYear();
         this.ice_mass = predictResult.getIce_mass();
         this.ice_mass_predict = predictResult.getIce_mass_predict();
+    }
+
+    private void makeTrashResult(Map<String, Object> todayResult) {
+        Map<String, Integer> temp = (Map<String, Integer>) todayResult.get("trashCount");
+        temp.keySet().stream().forEach(key -> {
+            trashType.add(key);
+            trashCount.add(temp.get(key));
+        });
     }
 }
