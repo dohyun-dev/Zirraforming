8017e241efdcae56956ca3a191715d6b03c2db1b
권도현 <tobeykwon@gmail.com>
2022-10-05 10:02:40 +0900

    S07P22D107-699 닉네임 validation 오류 수정
    Former-commit-id: c7cc6e36d0715f04222ab0515c3d17beebc737f7

--- ZirraForming/server/src/main/java/com/ssafy/server/api/dto/member/NicknameChangeRequest.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/api/dto/member/NicknameChangeRequest.java
@@ -8,8 +8,6 @@ import javax.validation.constraints.Size;
 
 @Data
 public class NicknameChangeRequest {
-    @NotBlank(message = "닉네임을 입력해주세요.")
-    @Size(min = 2, max = 10, message = "닉네임은 2자리이상 10자리로 작성하여야 합니다.")
-    @Pattern(regexp = "[\\w]+", message = "특수문자 사용은 불가능합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,6}$", message = "닉네임은 특수문자를 제외한 2~6자리여야 합니다.")
     private String nickname;
 }
