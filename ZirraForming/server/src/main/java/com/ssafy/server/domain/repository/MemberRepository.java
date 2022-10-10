2b0bfe143262d32a52a51886dd15d67907396f5c
권도현 <tobeykwon@gmail.com>
2022-10-11 05:57:16 +0900

    Zirraforming v1.3

--- ZirraForming/server/src/main/java/com/ssafy/server/domain/repository/MemberRepository.java
+++ ZirraForming/server/src/main/java/com/ssafy/server/domain/repository/MemberRepository.java
@@ -2,8 +2,11 @@ package com.ssafy.server.domain.repository;
 
 import com.ssafy.server.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByNickname(String nickname);
+
+    @Query("select m from Member m where m.id in :memberIds")
+    List<Member> findMembersById(@Param("memberIds") List<Long> memberIds);
 }
